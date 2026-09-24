5f3487454aa3314baaa2b5e7432e746cbaef4802
raffajah <rafaalghifari205@gmail.com>
2026-09-24 12:56:45 +0700

    code alamat and no hp mode

--- view/Latihan1View.php
+++ view/Latihan1View.php
@@ -11,7 +11,7 @@
     $i = 1;
 
     foreach($data as $mhs) {
-        echo $i++ . '. ' . $mhs['nama'] . ' ' . $mhs['nim'] . '</br>';
+        echo $i++ . '. ' . $mhs['nama'] . ' ' . $mhs['nim'] . ' ' . $mhs['alamat'] . ' ' . $mhs['no_hp'] . '</br>';
     }
     ?>
 </body>
