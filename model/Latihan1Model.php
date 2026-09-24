5759dc891d0d19c1453b83affc39a7c587104fd6
raffajah <rafaalghifari205@gmail.com>
2026-09-24 12:23:05 +0700

    model data latihanmodel

---
+++ model/Latihan1Model.php
@@ -0,0 +1,12 @@
+<?php
+class Latihan1Model {
+    public function getDataMhs() {
+        $data = [
+            ['nama' => 'Rafaa Alghifari', 'nim' => '064'],
+            ['nama' => 'John Doe', 'nim' => '065'],
+            ['nama' => 'Jane Smith', 'nim' => '066']
+        ];
+        return $data;
+    }
+}
+?>
