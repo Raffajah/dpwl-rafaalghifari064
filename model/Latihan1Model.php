8de1135f6204194936f5f53db2a16056675d9a86
raffajah <rafaalghifari205@gmail.com>
2026-09-24 12:55:44 +0700

    code alamat and no hp mode

--- model/Latihan1Model.php
+++ model/Latihan1Model.php
@@ -2,9 +2,9 @@
 class Latihan1Model {
     public function getDataMhs() {
         return [
-            ['nama' => 'Rafaa Alghifari', 'nim' => '064', 'alamat' => 'Jl. Contoh Alamat 1', 'no_hp' => '081234567890'],
-            ['nama' => 'John Doe', 'nim' => '065', 'alamat' => 'Jl. Contoh Alamat 2', 'no_hp' => '081234567891'],
-            ['nama' => 'Jane Smith', 'nim' => '066', 'alamat' => 'Jl. Contoh Alamat 3', 'no_hp' => '081234567892']
+            ['nama' => 'Rafaa Alghifari', 'nim' => '064', 'alamat' => 'Jl.Depati Amir ', 'no_hp' => '081234567890'],
+            ['nama' => 'John Doe', 'nim' => '065', 'alamat' => 'Mentok ', 'no_hp' => '081234567891'],
+            ['nama' => 'Jane Smith', 'nim' => '066', 'alamat' => 'kacang Pedang', 'no_hp' => '081234567892']
         ];
     }
 }
